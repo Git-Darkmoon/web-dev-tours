8b692e790f4e5fdcbc7d63a5008606dd6bd13ddf
Victicor_Torres <v.torres.04@live.com>
2023-08-11 18:18:56 -0500

    Overflow_y Hidden
    a

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
 // Navbar
 
-const back_btn = document.querySelector(".menu_back_btn")
-const burger_btn = document.querySelector(".burger")
-const menu = document.querySelector(".menu")
+const back_btn = document.querySelector(".menu_back_btn");
+const burger_btn = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+const body = document.querySelector("body");
 
-const API_URL = ("https://www.course-api.com/react-tours-project");
+const API_URL = "https://www.course-api.com/react-tours-project";
 
-async function dameTuCosita(){
-  const data=await fetch(API_URL);
-  const transform=await data.json();
+async function dameTuCosita() {
+  const data = await fetch(API_URL);
+  const transform = await data.json();
 
   console.log(transform);
   transform.map((eachTour) => {
-    const {name,info,image,price}=eachTour;
-    generateTours(name,info,image,price);
-  })
+    const { name, info, image, price } = eachTour;
+    generateTours(name, info, image, price);
+  });
 }
 
-dameTuCosita()
+dameTuCosita();
 
 burger_btn.addEventListener("click", () => {
-  menu.classList.add("openMenu")
-})
+  menu.classList.add("openMenu");
+  body.classList.add("openMenu");
+});
 back_btn.addEventListener("click", () => {
-  menu.classList.remove("openMenu")
-})
+  menu.classList.remove("openMenu");
+  body.classList.remove("openMenu");
+});
 
 // Tours data:
 
 const container = document.querySelector(".toursData");
 
-const generateTours = ( name, info, image, price)=>{
+const generateTours = (name, info, image, price) => {
   container.innerHTML += `
 
   <article class="card">
@@ -39,6 +42,5 @@ const generateTours = ( name, info, image, price)=>{
   <p class="desc">${info.slice(0, 200)}</p>
   <h4 class="price">$ ${price}</h4>
 </article>
-  `
-}
-
+  `;
+};
