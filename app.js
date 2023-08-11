b436ccce143f9c5b201d522481f98aa8b487bacf
Nicolas Mayorga <namayorgav@gmail.com>
2023-08-10 22:31:31 -0500

    tours mapping.

--- app.js
+++ app.js
@@ -34,15 +34,37 @@ const tours = [
     image: "https://www.course-api.com/images/tours/tour-5.jpeg",
     price: "2,595",
   },
-];
+]
 
-const back_btn = document.querySelector(".menu_back_btn");
-const burger_btn = document.querySelector(".burger");
-const menu = document.querySelector(".menu");
+// Navbar
+
+const back_btn = document.querySelector(".menu_back_btn")
+const burger_btn = document.querySelector(".burger")
+const menu = document.querySelector(".menu")
 
 burger_btn.addEventListener("click", () => {
-  menu.classList.add("openMenu");
-});
+  menu.classList.add("openMenu")
+})
 back_btn.addEventListener("click", () => {
-  menu.classList.remove("openMenu");
-});
+  menu.classList.remove("openMenu")
+})
+
+// Tours data:
+
+const container = document.querySelector(".toursData")
+
+tours.map((eachTour) => {
+  const { name, info, image, price } = eachTour
+
+  container.innerHTML += `
+  
+      <article class="card">
+          <img src="${image}" alt="" />
+          <h2>${name}</h2>
+          <p class="desc">
+            ${info.slice(0, 200)}
+          </p>
+          <h4 class="price">$ ${price}</h4>
+      </article>
+  `
+})
